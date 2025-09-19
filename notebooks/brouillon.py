57cc735d19f2a6fd2738d3ce59325ce1d9f46f4e
Kevin A. FIOVI <kevin.juniordatascientist@gmail.com>
2025-09-19 16:01:37 +0200

    Update and rename vide to brouillon.py

---
+++ notebooks/brouillon.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0, 2*np.pi, 400)
+y = np.sin(x)
+
+plt.plot(x, y, label="sin(x)")
+plt.title("Courbe sinusoïdale")
+plt.xlabel("x (radians)")
+plt.ylabel("y")
+plt.legend()
+plt.grid(True, alpha=0.3)
+plt.show()
